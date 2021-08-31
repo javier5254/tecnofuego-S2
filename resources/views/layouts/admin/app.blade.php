1a642717f9033ccf5618e4dd9c0fc9d2d38bb07a
JavierMoraBIAI <70889769+JavierMoraBIAI@users.noreply.github.com>
2021-08-31 16:20:50 -0500

    update

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -6,6 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>SIDERAC 2.0</title>
 
     <!-- Favicon -->
