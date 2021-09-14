b3dbe616f9e9f12d405baf4bd65c1313c2e5858d
JavierMoraBIAI <70889769+JavierMoraBIAI@users.noreply.github.com>
2021-09-14 15:25:35 -0500

    update

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -13,9 +13,10 @@ class ExampleTest extends TestCase
      * @return void
      */
     public function testBasicTest()
-    {
+    {   
+        
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+        $response->assertStatus(302);
     }
 }
