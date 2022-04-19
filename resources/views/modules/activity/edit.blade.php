6ab48bf2a7d7769fa5561cdfc78a74786015edb8
JavierDV <javier.mora@datavalue.ai>
2022-04-19 15:13:04 -0500

    update

--- resources/views/modules/activity/edit.blade.php
+++ resources/views/modules/activity/edit.blade.php
@@ -7474,10 +7474,9 @@
                     $('body').removeClass(
                         'modal-open'); //eliminamos la clase del body para poder hacer scroll
                     $('.modal-backdrop').remove();
-                    // var url = "{{ route('activity.main', 'id') }}";
-                    //     url = url.replace('id', type_id);
-                    //     window.location.href = url;
-                    window.location.href = "activity/"+type_id+"/main";
+                    var url = "{{ route('activity.main', 'id') }}";
+                    url = url.replace('id', type_id);
+                    window.location.href = url;
 
                 }
             });
